906582631a8e9f823f5a15a100d328623373dad6
Claudio <claudio.junior14@hotmail.com>
2019-09-11 13:36:19 -0300

    Mobile finished

--- tindev/android/app/src/main/java/com/tindev/MainActivity.java
+++ tindev/android/app/src/main/java/com/tindev/MainActivity.java
@@ -15,13 +15,14 @@ public class MainActivity extends ReactActivity {
     protected String getMainComponentName() {
         return "tindev";
     }
+
     @Override
     protected ReactActivityDelegate createReactActivityDelegate() {
     return new ReactActivityDelegate(this, getMainComponentName()) {
-      @Override
-      protected ReactRootView createRootView() {
-       return new RNGestureHandlerEnabledRootView(MainActivity.this);
-      }
+        @Override
+        protected ReactRootView createRootView() {
+        return new RNGestureHandlerEnabledRootView(MainActivity.this);
+        }
     };
-  }
+    }
 }
